b66e1c1d942a7e7a2f1f3feea7df1ba224e2b1d6
minnji88 <kkhhjj3393@gmail.com>
2021-01-26 20:56:09 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class component 안에서는 HTML 처럼 보이지만 JSX 문법사용
+// class 가 아니라 className 사용
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  // 비동기 
+  getMovies = async () => {
+    const { 
+      data : { 
+        data : { movies }}
+      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // console.log(movies);
+    this.setState({ movies, isLoading: false })
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+    <section className="container"> 
+      { isLoading ? (
+      <div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>
+      ) : (
+      <div className="movies">
+        { movies.map(movie => (
+          <Movie 
+          // 유일해야하므로 id 를 줘야한다.
+          key={movie.id}
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+          />
+      ))}
+      </div>
+      )}
+      </section>
+    );
+  }
 }
 
 export default App;
