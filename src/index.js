a5ae6dfb2723f5c4797fae87a7f1dc70e73b92b9
Minji Kim <62235737+minnji88@users.noreply.github.com>
2021-01-29 16:12:00 +0900

    🎬 movie

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//<App /> component: HTML 을 반환하는 함수 
+//<App /> component: HTML을 반환하는 함수 
 ReactDOM.render(<App />, document.getElementById('root'));
